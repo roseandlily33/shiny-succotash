184bb27660025c35407a6bd1cf123613750f5e4b
Victoria Benoit <vrose834@gmail.com>
2023-05-30 12:58:58 -0300

    Finished the controllers for now

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -5,42 +5,50 @@ module.exports = {
         try{
             const students = await Student.find();
             res.json(students);
-
         } catch(err){
             res.status(500).json(err);
         }
     },
     async getOneStudent(req, res){
         try{
-
+            const oneStudent = await Student.findOne({_id: req.params.studentId});
+            res.json(oneStudent);
         } catch(err){
             res.status(500).json(err);
         }
     },
     async createStudent(req, res){
         try{
-            
+            const newStudent = await Student.create(req.body);
+            res.json(newStudent);
         }catch(err){
             res.status(500).json(err);
         }
     },
     async deleteStudent(req, res){
         try{
-
+            const deleted = await Student.findByIdAndDelete({_id: req.params.studentId});
+            res.json(deleted);
         } catch(err){
             res.status(500).json(err);
         }
     },
     async addAssignment(req, res){
         try{
-
+            console.log(req.body, req.params);
+            const student = await Student.findOneAndUpdate({_id: req.params.studentId}, {$addToSet: {assignments: req.body}});
+            res.json(student);
         } catch(err){
             res.status(500).json(err);
         }
     },
     async deleteAssignment(req, res){
         try{
-
+            console.log(req.body, req.params)
+            const student = await Student.findOneAndUpdate({
+                _id: req.params.studentId
+            }, {$pull: {assignments:{ assignmentId: req.params.assignmentId}}})
+            res.json(student);
         } catch(err){
             res.status(500).json(err);
         }

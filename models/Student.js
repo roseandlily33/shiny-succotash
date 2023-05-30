d9ca6bc323792a05f6eba34fa393cb1a7e19714d
Victoria Benoit <vrose834@gmail.com>
2023-05-30 10:33:04 -0300

    Finished the student schema

---
+++ models/Student.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require('mongoose');
+const assignmentSchema = require('./Assignment');
+
+const studentSchema = new Schema(
+    {
+        first:{
+            type: String,
+            required: true,
+            maxlength: 50
+        }, 
+        last: {
+            type: String,
+            required: true,
+            maxlength: 50,
+        },
+        github: {
+            type: String,
+            required: true,
+            maxlength: 50,
+        },
+        assignments: [assignmentSchema]
+    }, {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
+const Student = model('student', studentSchema);
+module.exports = Student;
